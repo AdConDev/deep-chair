4105c2240e2db6799c485244cf69faa4cf886741
StealthSignatures <adrian.constante97@gmail.com>
2021-08-11 16:37:53 -0500

    facial recognition to resnet18

--- TransferLearning.py
+++ TransferLearning.py
@@ -15,13 +15,13 @@ import copy
 plt.ion()   # Modo interactivo
 # Data augmentation y normalización para entrenamiento
 data_transforms = {
-    'train': transforms.Compose([
+    'Entrenamiento': transforms.Compose([
+        transforms.CenterCrop(480),
         transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
-    'val': transforms.Compose([
+    'Validacion': transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
@@ -29,13 +29,13 @@ data_transforms = {
     ]),
 }
 
-data_dir = 'hymenoptera_data'
+data_dir = 'DataSetFacial'
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),data_transforms[x])
-                  for x in ['train', 'val']}
+                  for x in ['Entrenamiento', 'Validacion']}
 dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,shuffle=True, num_workers=4)
-              for x in ['train', 'val']}
-dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
-class_names = image_datasets['train'].classes
+              for x in ['Entrenamiento', 'Validacion']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['Entrenamiento', 'Validacion']}
+class_names = image_datasets['Entrenamiento'].classes
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -52,14 +52,13 @@ def imshow(inp, title=None):
     plt.pause(0.001)  # pausa para actualización de gráficas
 
 # Obtener batch de entrenamiento
-inputs, classes = next(iter(dataloaders['train']))
+inputs, classes = next(iter(dataloaders['Entrenamiento']))
 
 # Hacer grid del batch
 out = torchvision.utils.make_grid(inputs)
 imshow(out, title=[class_names[x] for x in classes])
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
-
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
 
@@ -67,38 +66,38 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
 
-        # Cada epoch tiene una fase de entrenamiento y de validación.
-        for phase in ['train', 'val']:
-            if phase == 'train':
+        # Each epoch has a training and validation phase
+        for phase in ['Entrenamiento', 'Validacion']:
+            if phase == 'Entrenamiento':
                 scheduler.step()
-                model.train()  # Modo entrenamiento
+                model.train()  # Set model to training mode
             else:
-                model.eval()   # Modo validación
+                model.eval()   # Set model to evaluate mode
 
             running_loss = 0.0
             running_corrects = 0
 
-            # Iterar sobre los datos.
+            # Iterate over data.
             for inputs, labels in dataloaders[phase]:
                 inputs = inputs.to(device)
                 labels = labels.to(device)
 
-                # parametros de gradientes en 0
+                # zero the parameter gradients
                 optimizer.zero_grad()
 
                 # forward
-                # obtener activaciones en modo entrenamiento
-                with torch.set_grad_enabled(phase == 'train'):
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'Entrenamiento'):
                     outputs = model(inputs)
                     _, preds = torch.max(outputs, 1)
                     loss = criterion(outputs, labels)
 
-                    # backward + optimización en modo entrenamiento
-                    if phase == 'train':
+                    # backward + optimize only if in training phase
+                    if phase == 'Entrenamiento':
                         loss.backward()
                         optimizer.step()
 
-                # estadística
+                # statistics
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
 
@@ -108,8 +107,8 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
 
-            # deep copy del modelo
-            if phase == 'val' and epoch_acc > best_acc:
+            # deep copy the model
+            if phase == 'Validacion' and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = copy.deepcopy(model.state_dict())
 
@@ -120,18 +119,18 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
         time_elapsed // 60, time_elapsed % 60))
     print('Best val Acc: {:4f}'.format(best_acc))
 
-    # cargar los mejores pesos del modelo
+    # load best model weights
     model.load_state_dict(best_model_wts)
     return model
 
-def visualize_model(model, num_images=6):
+def visualize_model(model, num_images=10):
     was_training = model.training
     model.eval()
     images_so_far = 0
     fig = plt.figure()
 
     with torch.no_grad():
-        for i, (inputs, labels) in enumerate(dataloaders['val']):
+        for i, (inputs, labels) in enumerate(dataloaders['Validacion']):
             inputs = inputs.to(device)
             labels = labels.to(device)
 
@@ -152,7 +151,7 @@ def visualize_model(model, num_images=6):
 
 model_ft = models.resnet18(pretrained=True)
 num_ftrs = model_ft.fc.in_features
-model_ft.fc = nn.Linear(num_ftrs, 2)
+model_ft.fc = nn.Linear(num_ftrs, 5)
 
 model_ft = model_ft.to(device)
 
@@ -173,7 +172,7 @@ for param in model_conv.parameters():
 
 # Parámetros de módulos creados recientemente tienen requires_grad=True por default.
 num_ftrs = model_conv.fc.in_features
-model_conv.fc = nn.Linear(num_ftrs, 2)
+model_conv.fc = nn.Linear(num_ftrs, 5)
 
 model_conv = model_conv.to(device)
 
@@ -191,3 +190,5 @@ visualize_model(model_conv)
 
 plt.ioff()
 plt.show()
+
+torch.save(model_ft.state_dict(), 'Resnet18Facial.pth')
